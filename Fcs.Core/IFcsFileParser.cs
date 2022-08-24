2db229125d7cdb3da4f27bcc0fe9429cc83de44f
Git X-Modules <support@tmatesoft.com>
2022-08-24 22:04:04 +0000

    Protecting root commit

--- Fcs.Core/IFcsFileParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Fcs.Core
-{
-    interface IFcsFileParser
-    {
-        FcsDataSet ReadDataSet(Stream stream, out long nextData, long fileBeginOffset = 0);
-    }
-}
